d50df1bc163e68d74d94808c88bf314d56c58667
Thomas Bernard <miniupnp@free.fr>
2019-05-02 14:25:00 +0200

    do not use usleep() which is obsolete
    4.3BSD, POSIX.1-2001.  POSIX.1-2001 declares usleep() obsolete;
    use nanosleep(2) instead.  POSIX.1-2008 removes the specification of
    usleep().
    
    As I think we don't have usage for finer than millisecond resolution,
    a custom msleep() function is defined instead, that calls nanosleep()
    
    In the UI code, we probably shouldn't call any sleep() function anyway,
    better wait for SDL2 input events.

---
+++ src/util/util_time.c
@@ -0,0 +1,21 @@
+#include "util_time.h"
+
+#include <time.h>
+#include <errno.h>
+
+unsigned int msleep(unsigned int ms)
+{
+	struct timespec req;
+	struct timespec rem;
+
+	req.tv_sec = ms / 1000;
+	req.tv_nsec = (ms % 1000) * 1000000L;
+	if (nanosleep(&req, &rem) < 0) {
+		if (errno == EINTR) {
+			/* interrupted by a signal */
+			return (rem.tv_sec * 1000) + (rem.tv_nsec / 1000000L);
+		}
+		return 0;
+	}
+	return 0;
+}
---
+++ src/util/util_time.h
@@ -0,0 +1,15 @@
+#ifndef H_UTIL_TIME
+#define H_UTIL_TIME
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Sleep for a specified number of milli-seconds */
+unsigned int msleep(unsigned int ms);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
